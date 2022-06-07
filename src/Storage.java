1394e331349e88e1cd0a57729ee90b16ae64fe23
ees1950 <99521582+ees1950@users.noreply.github.com>
2022-06-08 02:09:17 +0700

    Initial commit

---
+++ src/Storage.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Storage {
+    private Queue<Product> warehouseGoods = new LinkedList();
+    private int limitOfProducts;
+    private final Object locked = new Object();
+
+    public Storage(int limitOfProducts) {
+        this.limitOfProducts = limitOfProducts;
+    }
+
+    public Queue<Product> getWarehouseGoods() {
+        return warehouseGoods;
+    }
+
+    public int getLimitOfProducts() {
+        return limitOfProducts;
+    }
+
+    public Object getLocked() {
+        return locked;
+    }
+}
+
